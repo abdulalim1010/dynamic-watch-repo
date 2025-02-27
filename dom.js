983f9e416ba48f0a4ec1326fa371b1af75b00e67
abdulalilm1010 <abdulalim243457@gmail.com>
2025-02-27 23:52:42 +0600

    bad time

--- dom.js
+++ dom.js
@@ -1,1 +1,9 @@
-alert()
+document.getElementById('change-name').addEventListener('click', function () {
+      const changeAble = document.getElementById('box-line');
+  const changeAbles = changeAble.value;
+  
+      const finalChange = document.getElementById('final-change');
+   finalChange.innerText = changeAbles;
+
+  console.log('new Text',finalChanges)
+    })
